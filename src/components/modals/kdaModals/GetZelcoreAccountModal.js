21981b8a449d22ef25e595d3265167bd27a7c4e5
Felix <felice.angelini94@gmail.com>
2021-07-15 16:54:07 +0200

    added zelcore account modal and align layout with other modals

--- src/components/modals/kdaModals/GetZelcoreAccountModal.js
+++ src/components/modals/kdaModals/GetZelcoreAccountModal.js
@@ -1,15 +1,13 @@
 import React, { useState, useContext, useEffect } from "react";
 import styled from "styled-components/macro";
 import { Transition } from "react-spring/renderprops";
-import ModalContainer from "../../components/shared/ModalContainer";
-import Backdrop from "../../components/shared/Backdrop";
-import MyButton from "../../components/shared/Button";
-import { PactContext } from "../../contexts/PactContext";
-import getAccounts from "../../utils/getZelcoreAccts";
-import walletError from "../../components/alerts/walletError";
+import Backdrop from "../../../shared/Backdrop";
+import CustomButton from "../../../shared/CustomButton";
 import { Dropdown, Loader } from "semantic-ui-react";
-import reduceToken from "../../utils/reduceToken";
 import { Button } from "semantic-ui-react";
+import { PactContext } from "../../../contexts/PactContext";
+import getAccounts from "../../../utils/getZelcoreAccts";
+import reduceToken from "../../../utils/reduceToken";
 
 const Container = styled.div`
   position: absolute;
@@ -89,112 +87,72 @@ const GetZelcoreAccountModal = ({ show, onClose, onBack }) => {
   };
 
   return (
-    <Transition
-      items={show}
-      from={{ opacity: 0 }}
-      enter={{ opacity: 1 }}
-      leave={{ opacity: 0 }}
-    >
-      {(show) =>
-        show &&
-        ((props) => (
-          <Container style={props}>
-            <Backdrop
-              onClose={() => {
-                onClose();
-              }}
-            />
-            {!approved ? (
-              <ModalContainer
-                title="get zelcore accounts"
-                description="Please Approve"
-                containerStyle={{
-                  height: "100%",
-                  maxHeight: "80vh",
-                  maxWidth: "90vw",
-                }}
-                onClose={() => handleModalClose()}
-              >
-                <ContentContainer>
-                  <Text>
-                    Follow instructions in the wallet to share your accounts
-                  </Text>
-                </ContentContainer>
-                <ActionContainer>
-                  {loading ? (
-                    <Loader
-                      active
-                      inline="centered"
-                      style={{ color: "#FFFFFF" }}
-                    ></Loader>
-                  ) : (
-                    <MyButton
-                      onClick={() => {
-                        getAccountsFromWallet();
-                      }}
-                    >
-                      Retry
-                    </MyButton>
-                  )}
-                </ActionContainer>
-              </ModalContainer>
+    <>
+      {!approved ? (
+        <>
+          <Text>Follow instructions in the wallet to share your accounts</Text>
+          <ActionContainer>
+            {loading ? (
+              <Loader
+                active
+                inline="centered"
+                style={{ color: "#FFFFFF" }}
+              ></Loader>
             ) : (
-              <ModalContainer
-                title="get zelcore accounts"
-                description="Select Accounts"
-                containerStyle={{
-                  height: "100%",
-                  maxHeight: "80vh",
-                  maxWidth: "90vw",
+              <CustomButton
+                onClick={() => {
+                  getAccountsFromWallet();
                 }}
-                onClose={() => handleModalClose()}
               >
-                <ContentContainer>
-                  <Text>Choose Public Key you intend to use</Text>
-
-                  <Dropdown
-                    placeholder="More"
-                    fluid
-                    selection
-                    closeOnChange
-                    options={
-                      accounts &&
-                      accounts.map((item, index) => ({
-                        key: index,
-                        text: reduceToken(item),
-                        value: item,
-                      }))
-                    }
-                    onChange={handleDropdownChange}
-                    value={selectedAccount}
-                  />
-                </ContentContainer>
-                <ActionContainer>
-                  <Button.Group fluid>
-                    <MyButton
-                      border="none"
-                      boxShadow="none"
-                      background="transparent"
-                      onClick={() => handleCancel()}
-                    >
-                      Cancel
-                    </MyButton>
-                    <MyButton
-                      disabled={!selectedAccount}
-                      onClick={() => {
-                        handleConnect();
-                      }}
-                    >
-                      Connect
-                    </MyButton>
-                  </Button.Group>
-                </ActionContainer>
-              </ModalContainer>
+                Retry
+              </CustomButton>
             )}
-          </Container>
-        ))
-      }
-    </Transition>
+          </ActionContainer>
+        </>
+      ) : (
+        <>
+          <Text>Choose Public Key you intend to use</Text>
+
+          <Dropdown
+            placeholder="More"
+            fluid
+            selection
+            closeOnChange
+            options={
+              accounts &&
+              accounts.map((item, index) => ({
+                key: index,
+                text: reduceToken(item),
+                value: item,
+              }))
+            }
+            onChange={handleDropdownChange}
+            value={selectedAccount}
+          />
+
+          <ActionContainer>
+            <Button.Group fluid>
+              <CustomButton
+                border="none"
+                boxShadow="none"
+                background="transparent"
+                onClick={() => handleCancel()}
+              >
+                Cancel
+              </CustomButton>
+              <CustomButton
+                disabled={!selectedAccount}
+                onClick={() => {
+                  handleConnect();
+                }}
+              >
+                Connect
+              </CustomButton>
+            </Button.Group>
+          </ActionContainer>
+        </>
+      )}
+    </>
   );
 };
 
