03bbf18731d2e732e1939320e43851e4f7fe8526
Felix <felice.angelini94@gmail.com>
2021-07-20 16:11:19 +0200

    fix img

--- src/constants/cryptoCurrencies.js
+++ src/constants/cryptoCurrencies.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import { tokens } from './tokens'
+import React from "react";
+import { tokens } from "./tokens";
 
 const tokenData = tokens;
 
-Object.values(tokenData).forEach(token => {
-  tokenData[token.name].icon = <img src={tokenData[token.name].icon} style={{width: 20, height: 20, marginRight: '8px'}} />
-})
+Object.values(tokenData).forEach((token) => {
+  tokenData[token.name].icon = (
+    <img
+      alt=""
+      src={tokenData[token.name].icon}
+      style={{ width: 20, height: 20, marginRight: "8px" }}
+    />
+  );
+});
 
 export default tokenData;
