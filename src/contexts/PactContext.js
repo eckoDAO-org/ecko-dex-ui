acf185ec9d6c88969c89a655bea98447bf561739
Felix <felice.angelini94@gmail.com>
2021-07-20 09:40:03 +0200

    update PactContext

--- src/contexts/PactContext.js
+++ src/contexts/PactContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const PactContext = createContext();
 
@@ -10,15 +10,32 @@ export const PactProvider = (props) => {
     savedSlippage ? savedSlippage : 0.05
   );
   const [ttl, setTtl] = useState(savedTtl ? savedTtl : 600);
-
+  const [pairReserve, setPairReserve] = useState("");
+  const [ratio, setRatio] = useState(NaN);
   const storeSlippage = async (slippage) => {
     await setSlippage(slippage);
     await localStorage.setItem("slippage", slippage);
   };
 
+  useEffect(() => {
+    pairReserve
+      ? setRatio(pairReserve["token0"] / pairReserve["token1"])
+      : setRatio(NaN);
+  }, [pairReserve]);
+
   const storeTtl = async (ttl) => {
     await setTtl(slippage);
     await localStorage.setItem("ttl", ttl);
+  };
+
+  const getRatio = (toToken, fromToken) => {
+    if (toToken === fromToken) return 1;
+    return pairReserve["token1"] / pairReserve["token0"];
+  };
+
+  const getRatio1 = (toToken, fromToken) => {
+    if (toToken === fromToken) return 1;
+    return pairReserve["token0"] / pairReserve["token1"];
   };
 
   const contextValues = {
@@ -28,6 +45,8 @@ export const PactProvider = (props) => {
     ttl,
     setTtl,
     storeTtl,
+    getRatio,
+    getRatio1,
   };
   return (
     <PactContext.Provider value={contextValues}>
