0552a3b36729851b518f0cd72a5e42ec9479b7c0
Felix <felice.angelini94@gmail.com>
2021-07-22 16:46:42 +0200

    fix liquidity context

--- src/contexts/LiquidityContext.js
+++ src/contexts/LiquidityContext.js
@@ -1,19 +1,107 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, createContext } from "react";
 import pairTokens from "../constants/pairs.json";
 import Pact from "pact-lang-api";
+import { PactContext } from "./PactContext";
+import {
+  chainId,
+  creationTime,
+  GAS_PRICE,
+  network,
+} from "../constants/contextConstants";
 
-export const LiquidityContext = React.createContext(null);
+export const LiquidityContext = createContext(null);
 
 export const LiquidityProvider = (props) => {
+  const pact = useContext(PactContext);
   const [liquidityProviderFee, setLiquidityProviderFee] = useState(0.003);
-  const [pairList, setPairList] = useState(pairTokens);
   const [pairListAccount, setPairListAccount] = useState(pairTokens);
 
-  const contextValue = {};
+  const getPairListAccountBalance = async (account) => {
+    try {
+      const tokenPairList = Object.keys(pact.pairList).reduce((accum, pair) => {
+        accum += `[${pair.split(":").join(" ")}] `;
+        return accum;
+      }, "");
+      let data = await Pact.fetch.local(
+        {
+          pactCode: `
+            (namespace 'free)
+
+            (module kswap-read G
+
+              (defcap G ()
+                true)
+
+              (defun pair-info (pairList:list)
+                (let* (
+                  (token0 (at 0 pairList))
+                  (token1 (at 1 pairList))
+                  (p (kswap.exchange.get-pair token0 token1))
+                  (reserveA (kswap.exchange.reserve-for p token0))
+                  (reserveB (kswap.exchange.reserve-for p token1))
+                  (totalBal (kswap.tokens.total-supply (kswap.exchange.get-pair-key token0 token1)))
+                  (acctBal
+                      (try 0.0 (kswap.tokens.get-balance (kswap.exchange.get-pair-key token0 token1) ${JSON.stringify(
+                        account
+                      )})
+                    ))
+                )
+                [(kswap.exchange.get-pair-key token0 token1)
+                 reserveA
+                 reserveB
+                 totalBal
+                 acctBal
+                 (* reserveA (/ acctBal totalBal))
+                 (* reserveB (/ acctBal totalBal))
+               ]
+              ))
+            )
+            (map (kswap-read.pair-info) [${tokenPairList}])
+             `,
+          meta: Pact.lang.mkMeta(
+            "",
+            chainId,
+            GAS_PRICE,
+            3000,
+            creationTime(),
+            600
+          ),
+        },
+        network
+      );
+      if (data.result.status === "success") {
+        let dataList = data.result.data.reduce((accum, data) => {
+          accum[data[0]] = {
+            balance: data[4],
+            supply: data[3],
+            reserves: [data[1], data[2]],
+            pooledAmount: [data[5], data[6]],
+          };
+          return accum;
+        }, {});
+        const pairList = Object.values(pairTokens).map((pair) => {
+          return {
+            ...pair,
+            ...dataList[pair.name],
+          };
+        });
+        setPairListAccount(pairList);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const contextValue = {
+    pairListAccount,
+    setPairListAccount,
+    getPairListAccountBalance,
+  };
 
   return (
-    <LiquidityProvider.Provider value={contextValue}>
+    <LiquidityContext.Provider value={contextValue}>
       {props.children}
-    </LiquidityProvider.Provider>
+    </LiquidityContext.Provider>
   );
 };
+
+export const LiquidityConsumer = LiquidityContext.Consumer;
