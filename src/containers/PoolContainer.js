a83624650cd868d8633f038a2ef21223ba0ff033
Felix <felice.angelini94@gmail.com>
2021-07-27 18:04:36 +0200

    added Pool Container

--- src/containers/PoolContainer.js
+++ src/containers/PoolContainer.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import LiquidityContainer from "./liquidity/LiquidityContainer";
+import LiquidityList from "./liquidity/LiquidityList";
+import RemoveLiqContainer from "./liquidity/RemoveLiqContainer";
 
 const PoolContainer = () => {
-  return <div>PoolContainer</div>;
+  const [selectedView, setSelectedView] = useState(false);
+  const [pair, setPair] = useState(null);
+
+  return (
+    <>
+      {selectedView === "Remove Liquidity" ? (
+        <RemoveLiqContainer
+          closeLiquidity={() => setSelectedView(false)}
+          selectedView={selectedView}
+          pair={pair}
+        />
+      ) : selectedView ? (
+        <LiquidityContainer
+          closeLiquidity={() => setSelectedView(false)}
+          selectedView={selectedView}
+          setSelectedView={setSelectedView}
+          pair={pair}
+        />
+      ) : (
+        <LiquidityList
+          selectCreatePair={() => setSelectedView("Create A Pair")}
+          selectAddLiquidity={() => setSelectedView("Add Liquidity")}
+          selectRemoveLiquidity={() => setSelectedView("Remove Liquidity")}
+          selectPreviewLiquidity={() => setSelectedView("Preview Liquidity")}
+          setTokenPair={(pair) => setPair(pair)}
+        />
+      )}
+    </>
+  );
 };
 
 export default PoolContainer;
