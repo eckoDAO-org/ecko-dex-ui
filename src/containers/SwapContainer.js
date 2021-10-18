d8bc606d9ad0a2ca9ca5e1f1142b5bb8be42c693
Felix <felice.angelini94@gmail.com>
2021-10-18 17:54:15 +0200

    fix after merge

--- src/containers/SwapContainer.js
+++ src/containers/SwapContainer.js
@@ -15,7 +15,7 @@ import { SwapContext } from "../contexts/SwapContext";
 import { WalletContext } from "../contexts/WalletContext";
 import theme from "../styles/theme";
 import { getCorrectBalance, reduceBalance } from "../utils/reduceBalance";
-import ConnectWalletModal from '../components/modals/kdaModals/ConnectWalletModal';
+import ConnectWalletModal from "../components/modals/kdaModals/ConnectWalletModal";
 
 const Container = styled.div`
   width: 100%;
@@ -49,368 +49,311 @@ const SwapContainer = () => {
   const pact = useContext(PactContext);
   const swap = useContext(SwapContext);
   const account = useContext(AccountContext);
+  const wallet = useContext(WalletContext);
+
   const { gameEditionView } = useContext(GameEditionContext);
-	const { openModal } = useContext(GameEditionContext);
+  const { openModal } = useContext(GameEditionContext);
 
-	const [tokenSelectorType, setTokenSelectorType] = useState(null);
-	const [selectedToken, setSelectedToken] = useState(null);
-	const [fromValues, setFromValues] = useState({
-		amount: '',
-		balance: '',
-		coin: '',
-		address: '',
-		precision: 0,
-	});
+  const [tokenSelectorType, setTokenSelectorType] = useState(null);
+  const [selectedToken, setSelectedToken] = useState(null);
+  const [fromValues, setFromValues] = useState({
+    amount: "",
+    balance: "",
+    coin: "",
+    address: "",
+    precision: 0,
+  });
 
-	const [toValues, setToValues] = useState({
-		amount: '',
-		balance: '',
-		coin: '',
-		address: '',
-		precision: 0,
-	});
+  const [toValues, setToValues] = useState({
+    amount: "",
+    balance: "",
+    coin: "",
+    address: "",
+    precision: 0,
+  });
 
-	const [inputSide, setInputSide] = useState('');
-	const [fromNote, setFromNote] = useState('');
-	const [toNote, setToNote] = useState('');
-	const [showTxModal, setShowTxModal] = useState(false);
-	const [loading, setLoading] = useState(false);
-	const [fetchingPair, setFetchingPair] = useState(false);
-	const [noLiquidity, setNoLiquidity] = useState(false);
-	const [priceImpact, setPriceImpact] = useState('');
+  const [inputSide, setInputSide] = useState("");
+  const [fromNote, setFromNote] = useState("");
+  const [toNote, setToNote] = useState("");
+  const [showTxModal, setShowTxModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [fetchingPair, setFetchingPair] = useState(false);
+  const [noLiquidity, setNoLiquidity] = useState(false);
+  const [priceImpact, setPriceImpact] = useState("");
 
-	useEffect(() => {
-		if (!isNaN(fromValues.amount)) {
-			if (inputSide === 'from' && fromValues.amount !== '') {
-				setToNote('(estimated)');
-				setFromNote('');
-				setInputSide(null);
-				if (
-					fromValues.coin !== '' &&
-					toValues.coin !== '' &&
-					!isNaN(pact.ratio)
-				) {
-					if (fromValues.amount.length < 5) {
-						throttle(
-							500,
-							setToValues({
-								...toValues,
-								amount: reduceBalance(
-									// fromValues.amount / pact.ratio,
-									pact.computeOut(fromValues.amount),
-									toValues.precision
-								),
-							})
-						);
-					} else {
-						debounce(
-							500,
-							setToValues({
-								...toValues,
-								amount: reduceBalance(
-									// fromValues.amount / pact.ratio,
-									pact.computeOut(fromValues.amount),
-									toValues.precision
-								).toFixed(toValues.precision),
-							})
-						);
-					}
-				}
-			}
-			if (isNaN(pact.ratio) || fromValues.amount === '') {
-				setToValues((prev) => ({ ...prev, amount: '' }));
-			}
-		}
-	}, [fromValues.amount]);
+  useEffect(() => {
+    if (!isNaN(fromValues.amount)) {
+      if (inputSide === "from" && fromValues.amount !== "") {
+        setToNote("(estimated)");
+        setFromNote("");
+        setInputSide(null);
+        if (
+          fromValues.coin !== "" &&
+          toValues.coin !== "" &&
+          !isNaN(pact.ratio)
+        ) {
+          if (fromValues.amount.length < 5) {
+            throttle(
+              500,
+              setToValues({
+                ...toValues,
+                amount: reduceBalance(
+                  // fromValues.amount / pact.ratio,
+                  pact.computeOut(fromValues.amount),
+                  toValues.precision
+                ),
+              })
+            );
+          } else {
+            debounce(
+              500,
+              setToValues({
+                ...toValues,
+                amount: reduceBalance(
+                  // fromValues.amount / pact.ratio,
+                  pact.computeOut(fromValues.amount),
+                  toValues.precision
+                ).toFixed(toValues.precision),
+              })
+            );
+          }
+        }
+      }
+      if (isNaN(pact.ratio) || fromValues.amount === "") {
+        setToValues((prev) => ({ ...prev, amount: "" }));
+      }
+    }
+  }, [fromValues.amount]);
 
-	useEffect(() => {
-		if (!isNaN(toValues.amount)) {
-			if (inputSide === 'to' && toValues.amount !== '') {
-				setFromNote('(estimated)');
-				setToNote('');
-				setInputSide(null);
-				if (
-					fromValues.coin !== '' &&
-					toValues.coin !== '' &&
-					!isNaN(pact.ratio)
-				) {
-					if (toValues.amount.length < 5) {
-						throttle(
-							500,
-							setFromValues({
-								...fromValues,
-								amount: reduceBalance(
-									// toValues.amount * pact.ratio,
-									pact.computeIn(toValues.amount),
-									fromValues.precision
-								),
-							})
-						);
-						throttle(500, safeSetFrom(), fromValues.precision);
-					} else {
-						debounce(
-							500,
-							setFromValues({
-								...fromValues,
-								amount: reduceBalance(
-									// toValues.amount * pact.ratio,
-									pact.computeIn(toValues.amount),
-									fromValues.precision
-								).toFixed(fromValues.precision),
-							})
-						);
-						debounce(500, safeSetFrom(), fromValues.precision);
-					}
-				}
-			}
-			if (isNaN(pact.ratio) || toValues.amount === '') {
-				setFromValues((prev) => ({ ...prev, amount: '' }));
-			}
-		}
-	}, [toValues.amount]);
+  useEffect(() => {
+    if (!isNaN(toValues.amount)) {
+      if (inputSide === "to" && toValues.amount !== "") {
+        setFromNote("(estimated)");
+        setToNote("");
+        setInputSide(null);
+        if (
+          fromValues.coin !== "" &&
+          toValues.coin !== "" &&
+          !isNaN(pact.ratio)
+        ) {
+          if (toValues.amount.length < 5) {
+            throttle(
+              500,
+              setFromValues({
+                ...fromValues,
+                amount: reduceBalance(
+                  // toValues.amount * pact.ratio,
+                  pact.computeIn(toValues.amount),
+                  fromValues.precision
+                ),
+              })
+            );
+            throttle(500, safeSetFrom(), fromValues.precision);
+          } else {
+            debounce(
+              500,
+              setFromValues({
+                ...fromValues,
+                amount: reduceBalance(
+                  // toValues.amount * pact.ratio,
+                  pact.computeIn(toValues.amount),
+                  fromValues.precision
+                ).toFixed(fromValues.precision),
+              })
+            );
+            debounce(500, safeSetFrom(), fromValues.precision);
+          }
+        }
+      }
+      if (isNaN(pact.ratio) || toValues.amount === "") {
+        setFromValues((prev) => ({ ...prev, amount: "" }));
+      }
+    }
+  }, [toValues.amount]);
 
-	useEffect(() => {
-		if (!isNaN(pact.ratio)) {
-			if (fromValues.amount !== '' && toValues.amount === '') {
-				setToValues({
-					...toValues,
-					amount: reduceBalance(
-						pact.computeOut(fromValues.amount),
-						toValues.precision
-					),
-				});
-			}
-			if (fromValues.amount === '' && toValues.amount !== '') {
-				setFromValues({
-					...fromValues,
-					amount: reduceBalance(
-						pact.computeIn(toValues.amount),
-						fromValues.precision
-					),
-				});
-			}
-			if (fromValues.amount !== '' && toValues.amount !== '') {
-				setToValues({
-					...toValues,
-					amount: reduceBalance(
-						pact.computeOut(fromValues.amount),
-						toValues.precision
-					),
-				});
-			}
-		}
-	}, [pact.ratio]);
+  useEffect(() => {
+    if (!isNaN(pact.ratio)) {
+      if (fromValues.amount !== "" && toValues.amount === "") {
+        setToValues({
+          ...toValues,
+          amount: reduceBalance(
+            pact.computeOut(fromValues.amount),
+            toValues.precision
+          ),
+        });
+      }
+      if (fromValues.amount === "" && toValues.amount !== "") {
+        setFromValues({
+          ...fromValues,
+          amount: reduceBalance(
+            pact.computeIn(toValues.amount),
+            fromValues.precision
+          ),
+        });
+      }
+      if (fromValues.amount !== "" && toValues.amount !== "") {
+        setToValues({
+          ...toValues,
+          amount: reduceBalance(
+            pact.computeOut(fromValues.amount),
+            toValues.precision
+          ),
+        });
+      }
+    }
+  }, [pact.ratio]);
 
-	useEffect(() => {
-		if (!isNaN(pact.ratio)) {
-			setPriceImpact(
-				pact.computePriceImpact(
-					Number(fromValues.amount),
-					Number(toValues.amount)
-				)
-			);
-		} else {
-			setPriceImpact('');
-		}
-	}, [
-		fromValues.coin,
-		toValues.coin,
-		fromValues.amount,
-		toValues.amount,
-		pact.ratio,
-	]);
+  useEffect(() => {
+    if (!isNaN(pact.ratio)) {
+      setPriceImpact(
+        pact.computePriceImpact(
+          Number(fromValues.amount),
+          Number(toValues.amount)
+        )
+      );
+    } else {
+      setPriceImpact("");
+    }
+  }, [
+    fromValues.coin,
+    toValues.coin,
+    fromValues.amount,
+    toValues.amount,
+    pact.ratio,
+  ]);
 
-	useEffect(() => {
-		const getBalance = async () => {
-			if (
-				account.account &&
-				toValues.coin !== '' &&
-				fromValues.coin !== ''
-			) {
-				let acctOfFromValues = await account.getTokenAccount(
-					tokenData[fromValues.coin]?.code,
-					account.account.account,
-					tokenSelectorType === 'from'
-				);
-				let acctOfToValues = await account.getTokenAccount(
-					tokenData[toValues.coin]?.code,
-					account.account.account,
-					tokenSelectorType === 'to'
-				);
-				if (acctOfFromValues) {
-					let balanceFrom = getCorrectBalance(
-						acctOfFromValues.balance
-					);
-					setFromValues((prev) => ({
-						...prev,
-						balance: balanceFrom,
-					}));
-				}
-				if (acctOfToValues) {
-					let balanceTo = getCorrectBalance(acctOfToValues.balance);
-					setToValues((prev) => ({
-						...prev,
-						balance: balanceTo,
-					}));
-				}
-			}
-		};
-		getBalance();
-	}, [toValues.amount, fromValues.amount]);
+  useEffect(() => {
+    const getBalance = async () => {
+      if (account.account && toValues.coin !== "" && fromValues.coin !== "") {
+        let acctOfFromValues = await account.getTokenAccount(
+          tokenData[fromValues.coin]?.code,
+          account.account.account,
+          tokenSelectorType === "from"
+        );
+        let acctOfToValues = await account.getTokenAccount(
+          tokenData[toValues.coin]?.code,
+          account.account.account,
+          tokenSelectorType === "to"
+        );
+        if (acctOfFromValues) {
+          let balanceFrom = getCorrectBalance(acctOfFromValues.balance);
+          setFromValues((prev) => ({
+            ...prev,
+            balance: balanceFrom,
+          }));
+        }
+        if (acctOfToValues) {
+          let balanceTo = getCorrectBalance(acctOfToValues.balance);
+          setToValues((prev) => ({
+            ...prev,
+            balance: balanceTo,
+          }));
+        }
+      }
+    };
+    getBalance();
+  }, [toValues.amount, fromValues.amount]);
 
-	useEffect(() => {
-		if (tokenSelectorType === 'from')
-			return setSelectedToken(fromValues.coin);
-		if (tokenSelectorType === 'to') return setSelectedToken(toValues.coin);
-		return setSelectedToken(null);
-	}, [tokenSelectorType]);
+  useEffect(() => {
+    if (tokenSelectorType === "from") return setSelectedToken(fromValues.coin);
+    if (tokenSelectorType === "to") return setSelectedToken(toValues.coin);
+    return setSelectedToken(null);
+  }, [tokenSelectorType]);
 
-	useEffect(() => {
-		const getReserves = async () => {
-			if (toValues.coin !== '' && fromValues.coin !== '') {
-				setFetchingPair(true);
-				await pact.getPair(fromValues.address, toValues.address);
-				await pact.getReserves(fromValues.address, toValues.address);
-				setFetchingPair(false);
-			}
-		};
-		getReserves();
-	}, [fromValues.coin, toValues.coin]);
+  useEffect(() => {
+    const getReserves = async () => {
+      if (toValues.coin !== "" && fromValues.coin !== "") {
+        setFetchingPair(true);
+        await pact.getPair(fromValues.address, toValues.address);
+        await pact.getReserves(fromValues.address, toValues.address);
+        setFetchingPair(false);
+      }
+    };
+    getReserves();
+  }, [fromValues.coin, toValues.coin]);
 
-	useEffect(() => {
-		if (swap.walletSuccess) {
-			setLoading(false);
-			setFromValues({ amount: '', balance: '', coin: '', address: '' });
-			setToValues({ amount: '', balance: '', coin: '', address: '' });
-			pact.setWalletSuccess(false);
-		}
-	}, [swap.walletSuccess]);
+  useEffect(() => {
+    if (swap.walletSuccess) {
+      setLoading(false);
+      setFromValues({ amount: "", balance: "", coin: "", address: "" });
+      setToValues({ amount: "", balance: "", coin: "", address: "" });
+      pact.setWalletSuccess(false);
+    }
+  }, [swap.walletSuccess]);
 
-	const swapValues = () => {
-		const from = { ...fromValues };
-		const to = { ...toValues };
-		setFromValues({ ...to });
-		setToValues({ ...from });
-		if (toNote === '(estimated)') {
-			setFromNote('(estimated)');
-			setToNote('');
-		}
-		if (fromNote === '(estimated)') {
-			setToNote('(estimated)');
-			setFromNote('');
-		}
-	};
+  const swapValues = () => {
+    const from = { ...fromValues };
+    const to = { ...toValues };
+    setFromValues({ ...to });
+    setToValues({ ...from });
+    if (toNote === "(estimated)") {
+      setFromNote("(estimated)");
+      setToNote("");
+    }
+    if (fromNote === "(estimated)") {
+      setToNote("(estimated)");
+      setFromNote("");
+    }
+  };
 
-	// Check if their is enough liquidity before setting the from amount
-	const safeSetFrom = () => {
-		setNoLiquidity(false);
-		if (0 >= pact.computeIn(toValues.amount)) {
-			setNoLiquidity(true);
-			setFromValues({
-				...fromValues,
-				amount: 0,
-			});
-		} else {
-			setFromValues({
-				...fromValues,
-				amount: reduceBalance(
-					pact.computeIn(toValues.amount),
-					fromValues.precision
-				),
-			});
-		}
-	};
+  // Check if their is enough liquidity before setting the from amount
+  const safeSetFrom = () => {
+    setNoLiquidity(false);
+    if (0 >= pact.computeIn(toValues.amount)) {
+      setNoLiquidity(true);
+      setFromValues({
+        ...fromValues,
+        amount: 0,
+      });
+    } else {
+      setFromValues({
+        ...fromValues,
+        amount: reduceBalance(
+          pact.computeIn(toValues.amount),
+          fromValues.precision
+        ),
+      });
+    }
+  };
 
-	const onTokenClick = async ({ crypto }) => {
-		let balance;
-		if (crypto.code === 'coin') {
-			if (account.account) {
-				balance = account.account.balance;
-			}
-		} else {
-			let acct = await account.getTokenAccount(
-				crypto.code,
-				account.account.account,
-				tokenSelectorType === 'from'
-			);
-			if (acct) {
-				balance = getCorrectBalance(acct.balance);
-			}
-		}
-		if (tokenSelectorType === 'from')
-			setFromValues((prev) => ({
-				...prev,
-				balance: balance,
-				coin: crypto.name,
-				address: crypto.code,
-				precision: crypto.precision,
-			}));
-		if (tokenSelectorType === 'to')
-			setToValues((prev) => ({
-				...prev,
-				balance: balance,
-				coin: crypto.name,
-				address: crypto.code,
-				precision: crypto.precision,
-			}));
-	};
+  const onTokenClick = async ({ crypto }) => {
+    let balance;
+    if (crypto.code === "coin") {
+      if (account.account) {
+        balance = account.account.balance;
+      }
+    } else {
+      let acct = await account.getTokenAccount(
+        crypto.code,
+        account.account.account,
+        tokenSelectorType === "from"
+      );
+      if (acct) {
+        balance = getCorrectBalance(acct.balance);
+      }
+    }
+    if (tokenSelectorType === "from")
+      setFromValues((prev) => ({
+        ...prev,
+        balance: balance,
+        coin: crypto.name,
+        address: crypto.code,
+        precision: crypto.precision,
+      }));
+    if (tokenSelectorType === "to")
+      setToValues((prev) => ({
+        ...prev,
+        balance: balance,
+        coin: crypto.name,
+        address: crypto.code,
+        precision: crypto.precision,
+      }));
+  };
 
-	const onWalletRequestViewModalClose = () => {
-		wallet.setIsWaitingForWalletAuth(false);
-		wallet.setWalletError(null);
-	};
-
-	return (
-		<Container>
-			<TokenSelectorModal
-				show={tokenSelectorType !== null}
-				selectedToken={selectedToken}
-				onTokenClick={onTokenClick}
-				fromToken={fromValues.coin}
-				toToken={toValues.coin}
-				onClose={() => setTokenSelectorType(null)}
-			/>
-			<TxView
-				show={showTxModal}
-				selectedToken={selectedToken}
-				onTokenClick={onTokenClick}
-				onClose={() => setShowTxModal(false)}
-			/>
-			<WalletRequestView
-				show={wallet.isWaitingForWalletAuth}
-				error={wallet.walletError}
-				onClose={() => onWalletRequestViewModalClose()}
-			/>
-			<TitleContainer>
-				<Title style={{ fontFamily: theme.fontFamily.bold }}>
-					Swap
-				</Title>
-			</TitleContainer>
-			<SwapForm
-				fromValues={fromValues}
-				setFromValues={setFromValues}
-				toValues={toValues}
-				setToValues={setToValues}
-				fromNote={fromNote}
-				toNote={toNote}
-				setTokenSelectorType={setTokenSelectorType}
-				setInputSide={setInputSide}
-				swapValues={swapValues}
-				setShowTxModal={setShowTxModal}
-			/>
-			{!isNaN(pact.ratio) &&
-			fromValues.amount &&
-			fromValues.coin &&
-			toValues.amount &&
-			toValues.coin ? (
-				<SwapResults
-					priceImpact={priceImpact}
-					fromValues={fromValues}
-					toValues={toValues}
-				/>
-			) : (
-				<></>
-			)}
+  const onWalletRequestViewModalClose = () => {
+    wallet.setIsWaitingForWalletAuth(false);
+    wallet.setWalletError(null);
+  };
 
   return (
     <Container gameEditionView={gameEditionView}>
@@ -477,7 +420,11 @@ const SwapContainer = () => {
       />
     </Container>
   );
-			{/* <button
+};
+
+export default SwapContainer;
+
+/* <button
 				onClick={() =>
 					openModal({
 						isVisible: true,
@@ -488,7 +435,4 @@ const SwapContainer = () => {
 				}
 			>
 				Ciao
-			</button> */}
-};
-
-export default SwapContainer;
+			</button> */
