dd44d8af502417d2536b09ecc5ba4a6b26bf24ab
Guglielmo Sportelli <sportelli.guglielmo@gmail.com>
2021-10-19 15:25:52 +0200

    [KDX-21]: completed 'add liquidity' page redesign + minor fixes in swap

--- src/shared/InputToken.js
+++ src/shared/InputToken.js
@@ -7,73 +7,75 @@ import { GameEditionContext } from '../contexts/GameEditionContext';
 import theme from '../styles/theme';
 
 const Container = styled.div`
-	position: absolute;
-	top: 13%;
-	right: 4px;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	min-width: ${({ theme: { inputTokenWidth } }) => `${inputTokenWidth}px`};
+    position: absolute;
+    top: 13%;
+    right: 4px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-width: ${({ theme: { inputTokenWidth } }) => `${inputTokenWidth}px`};
 `;
 
 const ElementsContainer = styled.div`
-	cursor: pointer;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	svg:first-child {
-		margin-right: 8px;
-	}
+    cursor: pointer;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    svg:first-child {
+        margin-right: 8px;
+    }
 
-	span {
-		font-size: 16px;
-		margin-right: 13px;
-		font: ${({ gameEditionView }) => {
-			if (gameEditionView)
-				return `normal normal normal 14px/17px ${theme.fontFamily.pressStartRegular}`;
-		}};
-		color: ${({ gameEditionView }) =>
-			gameEditionView ? `${theme.colors.black}` : '#fff'};
-	}
+    span {
+        font-size: 16px;
+        margin-right: 13px;
+        font: ${({ gameEditionView }) => {
+            if (gameEditionView)
+                return `normal normal normal 14px/17px ${theme.fontFamily.pressStartRegular}`;
+        }};
+        color: ${({ gameEditionView }) =>
+            gameEditionView ? `${theme.colors.black}` : '#fff'};
+    }
 `;
 
 const InputToken = ({ icon, code, onClick, onClickButton, disabledButton }) => {
-	const { gameEditionView } = useContext(GameEditionContext);
+    const { gameEditionView } = useContext(GameEditionContext);
 
-	return (
-		<Container gameEditionView={gameEditionView}>
-			<ElementsContainer
-				gameEditionView={gameEditionView}
-				onClick={onClick}
-			>
-				{!gameEditionView && <>{icon}</>}
+    return (
+        <Container gameEditionView={gameEditionView}>
+            <ElementsContainer
+                gameEditionView={gameEditionView}
+                onClick={onClick}
+            >
+                {!gameEditionView && <>{icon}</>}
 
-				<span>{code}</span>
-			</ElementsContainer>
-			{gameEditionView ? <DropdownGe /> : <ArrowDown />}
-			<CustomButton
-				buttonStyle={{
-					padding: 12,
-					marginLeft: 12,
-				}}
-				fontSize="8px"
-				onClick={onClickButton}
-				disabled={disabledButton}
-			>
-				MAX
-			</CustomButton>
-		</Container>
-	);
+                <span>{code}</span>
+            </ElementsContainer>
+            {gameEditionView ? <DropdownGe /> : <ArrowDown />}
+            <CustomButton
+                buttonStyle={{
+                    padding: 12,
+                    marginLeft: 12,
+                    textTransform: gameEditionView ? 'capitalize' : 'uppercase',
+                }}
+                border={gameEditionView && 'none'}
+                fontSize={gameEditionView ? '13px' : '8px'}
+                onClick={onClickButton}
+                disabled={disabledButton}
+            >
+                Max
+            </CustomButton>
+        </Container>
+    );
 };
 
 InputToken.propTypes = {
-	icon: PropTypes.element,
-	code: PropTypes.string,
+    icon: PropTypes.element,
+    code: PropTypes.string,
 };
 
 InputToken.defaultProps = {
-	icon: null,
-	code: '',
+    icon: null,
+    code: '',
 };
 
 export default InputToken;
