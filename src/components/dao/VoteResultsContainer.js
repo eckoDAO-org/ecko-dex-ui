4bd81880ed363240defe488af66a00a9a8e6802d
Felix <felice.angelini94@gmail.com>
2022-03-11 17:02:25 +0100

    added functions on Vote Results

--- src/components/dao/VoteResultsContainer.js
+++ src/components/dao/VoteResultsContainer.js
@@ -5,7 +5,7 @@ import { FlexContainer } from '../shared/FlexContainer';
 import Label from '../shared/Label';
 import ProgressBar from '../shared/ProgressBar';
 
-const VoteResultsContainer = () => {
+const VoteResultsContainer = ({ onClickYes, onClickNo }) => {
   return (
     <>
       <FlexContainer className="column" gap={10} withGradient desktopStyle={{ width: 268, height: 'min-content' }}>
@@ -14,10 +14,12 @@ const VoteResultsContainer = () => {
         </Label>
         <ProgressBar topLabelLeft="Yes" currentValue={2} maxValue={3} />
         <ProgressBar topLabelLeft="No" darkBar currentValue={1} maxValue={3} />
-        <CustomButton type="primary" buttonStyle={{ marginTop: 14 }}>
-          yes
+        <CustomButton type="primary" buttonStyle={{ marginTop: 14 }} onClick={onClickYes}>
+          YES
         </CustomButton>
-        <CustomButton type="primary">no</CustomButton>
+        <CustomButton type="primary" onClick={onClickNo}>
+          NO
+        </CustomButton>
       </FlexContainer>
     </>
   );
