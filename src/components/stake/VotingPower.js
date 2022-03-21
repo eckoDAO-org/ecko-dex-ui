346c8b86347821b8247e06c0643a17acb46c5b85
agostino <nappo.agostino@gmail.com>
2022-03-21 19:20:50 +0100

    react-equation

--- src/components/stake/VotingPower.js
+++ src/components/stake/VotingPower.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+import { Equation } from 'react-equation';
+import { FlexContainer } from '../shared/FlexContainer';
 import InfoPopup from '../shared/InfoPopup';
 import Label from '../shared/Label';
 import ProgressBar from '../shared/ProgressBar';
 import CommonWrapper from './CommonWrapper';
+
+const EquationContainer = styled(FlexContainer)`
+  svg {
+    path {
+      fill: ${({ theme: { colors } }) => colors.white};
+    }
+  }
+  .equation span {
+    color: ${({ theme: { colors } }) => colors.white};
+    font-family: ${({ theme: { fontFamily } }) => fontFamily.syncopate};
+  }
+`;
 
 const VotingPower = () => {
   return (
@@ -11,17 +26,19 @@ const VotingPower = () => {
       containerStyle={{ marginTop: 24 }}
       cardStyle={{ paddingBottom: 32 }}
       title={
-        <div>
+        <EquationContainer className="flex align-ce">
           <Label fontFamily="syncopate" fontSize={24}>
             Voting Power (V)
           </Label>
-        </div>
+          <Equation className="equation" value="V = root(2, P*M)" />
+        </EquationContainer>
       }
     >
       <div className="flex align-ce">
         <Label fontFamily="syncopate" fontSize={16}>
           multiplayer (m)
         </Label>
+
         <InfoPopup></InfoPopup>
       </div>
 
