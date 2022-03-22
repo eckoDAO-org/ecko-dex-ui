381bcb78ac63bbe7e2e4f96b94ef8b5a6361d5ad
agostino <nappo.agostino@gmail.com>
2022-03-22 16:09:42 +0100

    fix

--- src/components/stake/StakeInfo.js
+++ src/components/stake/StakeInfo.js
@@ -5,15 +5,13 @@ import List from '../shared/List';
 
 const StakeInfo = () => {
   return (
-    <FlexContainer className="colum" gap={16}>
-      <Label>
-        The Kaddex Staking tool accomplishes three main purposes: • Passive Income Source: stakers receive 0.05% from all swapping fees. Rewards
-        re-staking (“Compounding”) is available once a week from the end of your waiting time.
-      </Label>
+    <FlexContainer className="column" gap={16}>
+      <Label>The Kaddex Staking tool accomplishes three main purposes:</Label>
 
       <List
         paddingLeft={16}
         items={[
+          'Passive Income Source: stakers receive 0.05% from all swapping fees. Rewards re-staking (“Compounding”) is available once a week from the end of your waiting time.',
           'Users’ Empowerment: staking allows users to increase their DAO voting Power in a way that values long-term holders over whales.',
           "Deflation of KDX's overall supply: all the penalties coming from early unstaking are constantly burnt.",
         ]}
--- src/components/stake/UnstakeInfo.js
+++ src/components/stake/UnstakeInfo.js
@@ -11,10 +11,10 @@ const UnstakeInfo = () => {
         Unstaking your funds is always possible. To prevent smart contract manipulation and further reward long-term holders, an easy-to-understand
         penalty structure has been designed:
       </Label>
-
       <List
         paddingLeft={16}
         items={[
+          'Unstaking during the first 72 hours will trigger a 3% flat penalty on your staked amount.',
           'Withdrawing your rewards during the first 60 days will result in an exponentially time-decreasing penalty on your accumulated rewards –your initial capital will not be affected.',
         ]}
       />
@@ -22,9 +22,7 @@ const UnstakeInfo = () => {
       <EquationContainer>
         <Equation className="equation" value="p(d) = max (0, ((60-d)/60)^0.66)" />
       </EquationContainer>
-
       <Label>where:</Label>
-
       <List
         listType="circle"
         items={[
@@ -34,17 +32,12 @@ const UnstakeInfo = () => {
           '0“0.66” is an arbitrary coefficient used to model the penalty-curve efficiently.',
         ]}
       />
-
       <List paddingLeft={16} items={['Adding more KDX to your stake, will simply increase your waiting time proportionally']} />
-
       <Label>Waiting Time Formula: </Label>
-
       <EquationContainer>
         <Equation className="equation" value="w = (60*n + r*p)/(n+p)" />
       </EquationContainer>
-
       <Label>where:</Label>
-
       <List
         listType="circle"
         items={[
