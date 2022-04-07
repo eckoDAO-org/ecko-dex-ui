2fffad75cb5d579e787e80c95f8dc5d9dcaf7c00
Felix <felice.angelini94@gmail.com>
2022-04-07 11:57:56 +0200

    fixed context constants

--- src/constants/contextConstants.js
+++ src/constants/contextConstants.js
@@ -5,7 +5,7 @@ export const FEE = process.env.REACT_APP_KDA_FEE || 0.003;
 export const GAS_PRICE = Number(process.env.REACT_APP_KDA_GAS_PRICE) || 0.0000001;
 export const GAS_LIMIT = Number(process.env.REACT_APP_KDA_GAS_LIMIT) || 100000;
 export const NETWORK_TYPE = process.env.REACT_APP_KDA_NETWORK_TYPE || 'testnet';
-export const ENABLE_GAS_STATION = process.env.ENABLE_GAS_STATION || false;
+export const ENABLE_GAS_STATION = process.env.REACT_APP_ENABLE_GAS_STATION || false;
 
 export const NETWORK = `${process.env.REACT_APP_KDA_NETWORK}/chainweb/0.0/${NETWORKID}/chain/${CHAIN_ID}/pact`;
 export const creationTime = () => Math.round(new Date().getTime() / 1000) - 10;
