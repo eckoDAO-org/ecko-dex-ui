70523f8b028139cd1a27eb7c46e613e8f129458e
Guglielmo Sportelli <sportelli.guglielmo@gmail.com>
2022-04-21 17:28:55 +0200

    errors handled

--- src/components/dao/AllProposalsContainer.js
+++ src/components/dao/AllProposalsContainer.js
@@ -23,7 +23,9 @@ const AllProposalsContainer = ({ accountData }) => {
 
   const fetchData = async () => {
     const readAllProposalsRes = await readAllProposals();
-    const orderedProposals = readAllProposalsRes?.sort((x, y) => moment(y['start-date']?.time) - moment(x['start-date']?.time));
+    const orderedProposals = !readAllProposalsRes.errorMessage
+      ? readAllProposalsRes.sort((x, y) => moment(y['start-date']?.time) - moment(x['start-date']?.time))
+      : [];
     setAllProposal(orderedProposals);
     setDaoALlProposalsLoading(false);
   };
@@ -53,36 +55,44 @@ const AllProposalsContainer = ({ accountData }) => {
       <FlexContainer className="row" gap={16} mobileClassName="column-reverse" mobileStyle={{ paddingBottom: 16 }}>
         <FlexContainer className="column background-fill" withGradient style={{ height: 'min-content', maxHeight: 500, flex: 1 }}>
           <PartialScrollableScrollSection id="proposals-list" className="scrollbar-none" style={{ width: '100%' }}>
-            {allProposal.map((data, index) => (
-              <FlexContainer className="column pointer" key={index} onClick={() => history.push(ROUTE_DAO_PROPOSAL.replace(':proposal_id', data.id))}>
-                <FlexContainer className="align-ce" gap={8} style={{ marginBottom: 8 }}>
-                  <Label fontFamily="basier" fontSize={13} labelStyle={{ opacity: 0.7 }}>
-                    {moment(data['start-date']?.time).format('YYYY-MM-DD')}
-                  </Label>
-                  <Label
-                    fontFamily="basier"
-                    fontSize={10}
-                    color={'#fff'}
-                    labelStyle={{
-                      backgroundColor:
-                        moment(data['start-date'].time) <= moment() && moment(data['end-date'].time) >= moment()
-                          ? commonColors.active
-                          : commonColors.closed,
-                      borderRadius: 100,
-                      padding: '2px 8px',
-                    }}
-                  >
-                    {getStatusProposal(data)}
-                  </Label>
-                </FlexContainer>
+            {allProposal.length > 0 ? (
+              allProposal.map((data, index) => (
+                <FlexContainer
+                  className="column pointer"
+                  key={index}
+                  onClick={() => history.push(ROUTE_DAO_PROPOSAL.replace(':proposal_id', data.id))}
+                >
+                  <FlexContainer className="align-ce" gap={8} style={{ marginBottom: 8 }}>
+                    <Label fontFamily="basier" fontSize={13} labelStyle={{ opacity: 0.7 }}>
+                      {moment(data['start-date']?.time).format('YYYY-MM-DD')}
+                    </Label>
+                    <Label
+                      fontFamily="basier"
+                      fontSize={10}
+                      color={'#fff'}
+                      labelStyle={{
+                        backgroundColor:
+                          moment(data['start-date'].time) <= moment() && moment(data['end-date'].time) >= moment()
+                            ? commonColors.active
+                            : commonColors.closed,
+                        borderRadius: 100,
+                        padding: '2px 8px',
+                      }}
+                    >
+                      {getStatusProposal(data)}
+                    </Label>
+                  </FlexContainer>
 
-                <Label fontFamily="basier" fontSize={16} labelStyle={{ marginBottom: 4 }}>
-                  {data?.title}
-                </Label>
-                <HtmlFormatterContainer htmlText={data?.description} asAString />
-                {index < allProposal.length - 1 && <Divider />}
-              </FlexContainer>
-            ))}
+                  <Label fontFamily="basier" fontSize={16} labelStyle={{ marginBottom: 4 }}>
+                    {data?.title}
+                  </Label>
+                  <HtmlFormatterContainer htmlText={data?.description} asAString />
+                  {index < allProposal.length - 1 && <Divider />}
+                </FlexContainer>
+              ))
+            ) : (
+              <Label className="justify-ce">No proposals</Label>
+            )}
           </PartialScrollableScrollSection>
         </FlexContainer>
 
