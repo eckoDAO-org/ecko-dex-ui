cc0df71cccb7ecbf1138cc2c181694e5d16ac476
agostino <nappo.agostino@gmail.com>
2022-04-28 12:20:12 +0200

    changed staking text; minor fix in common table

--- src/components/stake/PenaltyRewardsInfo.js
+++ src/components/stake/PenaltyRewardsInfo.js
@@ -7,16 +7,16 @@ import List from '../shared/List';
 const PenaltyRewardsInfo = () => {
   return (
     <FlexContainer className="column" gap={16}>
-      <Label>
-        The penalty structure of the KDX Staking tool is designed to achieve two objectives: to reward long-term holders and to prevent manipulation
-        and spam of the smart contract involved.
-        <br />
-        <br />
-        If you unstake during the first 72hours you will incur in a penalty: 3% flat penalty on your staked amount. If you withdraw your rewards
-        during the first 60 days, you will incur in a penalty: the penalty will only affect your accumulated rewards and exponentially decreases in
-        time. Your initial capital will not be affected.
-      </Label>
+      <Label>To achieve 2 objectives: rewarding long-term holders and preventing manipulation and spamming of the smart contract involved.</Label>
 
+      <List
+        paddingLeft={16}
+        items={[
+          'If you unstake within the first 72 hours, you will incur into a 3% flat penalty on your staked amount.',
+          'If you withdraw your rewards within the first 60 days, the penalty will only affect your accumulated rewards and exponentially decreases in time.',
+        ]}
+      />
+      <Label>Your initial capital will not be affected.</Label>
       <EquationContainer className="flex align-ce">
         <Equation className="equation" value="p(d) = max (0, ((60-d)/60)^0.66)" />
       </EquationContainer>
@@ -25,9 +25,9 @@ const PenaltyRewardsInfo = () => {
       <List
         paddingLeft={16}
         items={[
-          'p denotes the penalty proportion on rewards accumulated on the first 60 days;',
+          'p denotes the penalty proportion on rewards accumulated in the first 60 days;',
           'd is the number of days the user has kept their KDX staked;',
-          '“60” is the minimum amount of days required to have no penalties;',
+          '“60”  is the minimum number of days required not to incur into any penalty;',
           '“0.66” is an arbitrary coefficient used to model the penalty-curve efficiently.',
         ]}
       />
