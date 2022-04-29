515eefd71fe3af522c867f7832d664c8256d1f4a
agostino <nappo.agostino@gmail.com>
2022-04-29 12:24:14 +0200

    updated account balance when connect wallet; fixed pool share in liquidity

--- src/constants/cryptoCurrencies.js
+++ src/constants/cryptoCurrencies.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { tokens } from './tokens';
+import { pairs } from './pairs';
 
 const environment = process.env.REACT_APP_KDA_NETWORK_TYPE || 'testnet';
 const tokenData = tokens[environment];
+const pairsData = pairs[environment];
 
 Object.values(tokenData).forEach((token) => {
   tokenData[token.name].icon = <img alt="" src={tokenData[token.name].icon} style={{ width: 20, height: 20, marginRight: '8px' }} />;
 });
 
+export const getPairByTokensName = (token0Name, token1Name) => {
+  return Object.values(pairsData).find(
+    (pair) => (pair.token0 === token0Name && pair.token1 === token1Name) || (pair.token0 === token1Name && pair.token1 === token0Name)
+  );
+};
 export default tokenData;
