44b6b26167a1fd110afa98cd33da069e2caa39b9
Felix <felice.angelini94@gmail.com>
2022-05-25 12:43:22 +0200

    restyling staked bar chart

--- src/components/shared/StackedBarChart.js
+++ src/components/shared/StackedBarChart.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import styled from 'styled-components';
 import { FlexContainer } from './FlexContainer';
@@ -8,6 +8,22 @@ const StackedBarChartContainer = styled(FlexContainer)`
   .recharts-yAxis,
   .recharts-cartesian-axis-line {
     display: none;
+  }
+
+  /* recharts-layer 
+  recharts-cartesian-axis-tick
+  recharts-cartesian-axis-tick-line 
+  
+  
+  var shortLine = document.getElementById('ShortLine')
+shortLine.y1.baseVal.value = 500
+  */
+
+  .recharts-cartesian-axis-tick {
+    line {
+      stroke: #ffffff;
+      height: 120;
+    }
   }
 `;
 
@@ -30,11 +46,23 @@ const StackedBarChart = ({}) => {
     );
   };
 
+  const ticks = document.getElementsByClassName('recharts-cartesian-axis-tick-line');
+  for (const element of ticks) {
+    if (element !== ticks[0] && element !== ticks[ticks.length - 1]) element.setAttribute('y2', -10);
+    else element.style.stroke = 'transparent';
+  }
+
+  // const tspan = document.getElementsByClassName('recharts-text recharts-cartesian-axis-tick-value');
+  // console.log('LOG --> tspan', tspan);
+  // for (const element of tspan) {
+  //   element.;
+  // }
+
   return (
     <StackedBarChartContainer withGradient className="background-fill w-100">
-      <ResponsiveContainer height={100} width={'100%'}>
+      <ResponsiveContainer height={80} width={'100%'}>
         <BarChart layout="vertical" data={data}>
-          <YAxis type="category" dataKey="name" stroke="#FFFFFF" fontSize="12" />
+          <YAxis type="category" dataKey="name" stroke="#FFFFFF" fontSize="12" width={0} />
           <Tooltip content={<CustomTooltip />} cursor={{ fill: 'transparent' }} />
           <Bar
             onMouseEnter={() => {
@@ -55,7 +83,7 @@ const StackedBarChart = ({}) => {
             fill="#76a8dd"
             stackId="a"
           />
-          <XAxis ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} domain={[10, 100]} type="number" />
+          <XAxis ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} dy={5} domain={[10, 100]} type="number" tickFormatter={(v) => `${v}%`} />
         </BarChart>
       </ResponsiveContainer>
     </StackedBarChartContainer>
