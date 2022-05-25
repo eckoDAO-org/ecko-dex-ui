3f67a63aa04b2bb840c764cf015b051d8a973cee
agostino <nappo.agostino@gmail.com>
2022-05-25 10:55:03 +0200

    created container

--- src/components/shared/StackedBarChart.js
+++ src/components/shared/StackedBarChart.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import styled from 'styled-components';
+import { FlexContainer } from './FlexContainer';
 import Label from './Label';
+
+const StackedBarChartContainer = styled(FlexContainer)`
+  .recharts-yAxis,
+  .recharts-cartesian-axis-line {
+    display: none;
+  }
+`;
 
 const TooltipContent = styled.div`
   background: ${({ theme: { colors } }) => colors.primary};
@@ -23,32 +31,34 @@ const StackedBarChart = ({}) => {
   };
 
   return (
-    <ResponsiveContainer height={100} width={'100%'}>
-      <BarChart layout="vertical" data={data} margin={{ left: 50, right: 50 }}>
-        <YAxis type="category" dataKey="name" stroke="#FFFFFF" fontSize="12" />
-        <Tooltip content={<CustomTooltip />} />
-        <Bar
-          onMouseEnter={() => {
-            setBarOnHover('completed');
-          }}
-          onMouseLeave={() => setBarOnHover('')}
-          radius={[10, 0, 0, 10]}
-          dataKey="completed"
-          fill="#82ba7f"
-          stackId="a"
-        />
-        <Bar onMouseEnter={() => setBarOnHover('failed')} onMouseLeave={() => setBarOnHover('')} dataKey="failed" fill="#dd7876" stackId="a" />
-        <Bar
-          onMouseEnter={() => setBarOnHover('inprogress')}
-          onMouseLeave={() => setBarOnHover('')}
-          radius={[0, 10, 10, 0]}
-          dataKey="inprogress"
-          fill="#76a8dd"
-          stackId="a"
-        />
-        <XAxis ticks={[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} domain={[10, 100]} type="number" />
-      </BarChart>
-    </ResponsiveContainer>
+    <StackedBarChartContainer withGradient className="background-fill w-100">
+      <ResponsiveContainer height={100} width={'100%'}>
+        <BarChart layout="vertical" data={data}>
+          <YAxis type="category" dataKey="name" stroke="#FFFFFF" fontSize="12" />
+          <Tooltip content={<CustomTooltip />} cursor={{ fill: 'transparent' }} />
+          <Bar
+            onMouseEnter={() => {
+              setBarOnHover('completed');
+            }}
+            onMouseLeave={() => setBarOnHover('')}
+            radius={[10, 0, 0, 10]}
+            dataKey="completed"
+            fill="#82ba7f"
+            stackId="a"
+          />
+          <Bar onMouseEnter={() => setBarOnHover('failed')} onMouseLeave={() => setBarOnHover('')} dataKey="failed" fill="#dd7876" stackId="a" />
+          <Bar
+            onMouseEnter={() => setBarOnHover('inprogress')}
+            onMouseLeave={() => setBarOnHover('')}
+            radius={[0, 10, 10, 0]}
+            dataKey="inprogress"
+            fill="#76a8dd"
+            stackId="a"
+          />
+          <XAxis ticks={[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]} domain={[10, 100]} type="number" />
+        </BarChart>
+      </ResponsiveContainer>
+    </StackedBarChartContainer>
   );
 };
 
