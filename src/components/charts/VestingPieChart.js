be950569ff11756341ca4b80404d393e70ef2f0e
Guglielmo Sportelli <sportelli.guglielmo@gmail.com>
2022-05-26 16:55:43 +0200

    fix pie chart info

--- src/components/charts/VestingPieChart.js
+++ src/components/charts/VestingPieChart.js
@@ -13,16 +13,20 @@ const VestingPieChart = ({ kdxSupply, KDX_TOTAL_SUPPLY }) => {
   const [width] = useWindowSize();
 
   const vesting = getVestingScheduleData('2021-06-01', moment().format('YYYY-MM-DD')).slice(-1)[0];
-  const notCirculating =
+  const notCirculatingValue =
     100 - (100 - vesting['Total Supply']) - vesting['Liquidity mining'] - vesting['Community Sales'] - vesting['Team'] - vesting['DAO treasury'];
+
   const chartData = {
     Burned: { name: 'Burned', color: '#ED1C1C', value: 100 - vesting['Total Supply'] },
     LiquidityMining: { name: 'Liquidity Mining', color: '#8884d8', value: vesting['Liquidity mining'] },
     CommunitySales: { name: 'Community Sales', color: '#ED1CB5', value: vesting['Community Sales'] },
     Team: { name: 'Team', color: '#D0A032', value: vesting['Team'] },
     DAOTreasury: { name: 'DAO Treasury', color: '#39FFFC', value: vesting['DAO treasury'] },
-    NotCirculation: { name: 'Not Circulating', color: '#9797A4', value: notCirculating },
   };
+
+  const Circulating = { name: 'Circulating', color: '#9797A4', value: 100 - notCirculatingValue };
+
+  const NotCirculating = { name: 'Not Circulating', color: '#9797A4', value: notCirculatingValue };
 
   const getPieSize = () => {
     if (width < 900) {
@@ -47,12 +51,12 @@ const VestingPieChart = ({ kdxSupply, KDX_TOTAL_SUPPLY }) => {
         <ResponsiveContainer width={getPieSize().size} height={getPieSize().size}>
           <PieChart>
             <Pie
-              data={Object.values(chartData).map((d) => ({ ...d }))}
+              data={Object.values({ ...chartData, NotCirculating }).map((d) => ({ ...d }))}
               innerRadius={getPieSize().innerRadius}
               outerRadius={getPieSize().outerRadius}
               dataKey="value"
             >
-              {Object.values(chartData).map((d, i) => (
+              {Object.values({ ...chartData, NotCirculating }).map((d, i) => (
                 <Cell key={i} fill={d.color} />
               ))}
             </Pie>
@@ -60,21 +64,26 @@ const VestingPieChart = ({ kdxSupply, KDX_TOTAL_SUPPLY }) => {
         </ResponsiveContainer>
 
         <FlexContainer className="flex column justify-sb" mobileClassName="column w-100" mobileStyle={{ marginTop: 24 }}>
-          {Object.values(chartData).map((d, i) => (
+          {Object.values({ ...chartData, Circulating }).map((d, i) => (
             <FlexContainer key={i} className="flex align-ce justify-sb" mobileStyle={{ marginBottom: 16 }} tabletStyle={{ marginBottom: 16 }}>
-              <Label fontSize={24} mobileFontSize={16} tabletFontSize={20} color={d.color} labelStyle={{ marginRight: 50 }}>
+              <Label fontSize={16} color={d.color} labelStyle={{ marginRight: 50 }}>
                 {d.name}:
               </Label>
-              <Label fontSize={24} mobileFontSize={16} tabletFontSize={20} color={d.color}>
+              <Label fontSize={16} color={d.color}>
                 {d.value.toFixed(2)}%
               </Label>
             </FlexContainer>
           ))}
-          <Divider fitted />
           <FlexContainer className="w-100 flex" mobileStyle={{ marginTop: 16 }} tabletStyle={{ marginTop: 16 }}>
             <ProgressBar maxValue={KDX_TOTAL_SUPPLY} currentValue={kdxSupply} />
-            <Label className="no-wrap" labelStyle={{ flex: 2, marginLeft: 20 }}>
-              {((100 * kdxSupply) / KDX_TOTAL_SUPPLY).toFixed(2)} %
+          </FlexContainer>
+          <Divider fitted />
+          <FlexContainer className="flex align-ce justify-sb" mobileStyle={{ marginBottom: 16 }} tabletStyle={{ marginBottom: 16 }}>
+            <Label fontSize={16} color={NotCirculating.color} labelStyle={{ marginRight: 50 }}>
+              {NotCirculating.name}:
+            </Label>
+            <Label fontSize={16} color={NotCirculating.color}>
+              {NotCirculating.value.toFixed(2)}%
             </Label>
           </FlexContainer>
         </FlexContainer>
