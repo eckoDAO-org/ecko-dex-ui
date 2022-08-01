b6aba464f499ab4147dfba0526d3709d7309fbbd
Felix <felice.angelini94@gmail.com>
2022-08-01 19:35:38 +0200

    add validation on coingecko api

--- src/api/coingecko.js
+++ src/api/coingecko.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 export const getCoingeckoUsdPrice = async (tokenName) => {
-  const API = `https://api.coingecko.com/api/v3/simple/price?ids=${tokenName}&vs_currencies=usd`;
+  if (tokenName) {
+    const API = `https://api.coingecko.com/api/v3/simple/price?ids=${tokenName}&vs_currencies=usd`;
 
-  return await axios
-    .get(API)
-    .then(async (res) => {
-      return res.data?.[tokenName]?.usd;
-    })
-    .catch((err) => console.log('error', err));
+    return await axios
+      .get(API)
+      .then(async (res) => {
+        return res.data?.[tokenName]?.usd;
+      })
+      .catch((err) => console.log('error', err));
+  } else return null;
 };
