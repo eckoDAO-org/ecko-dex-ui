da22bc0ac0904d544470764cd588aa726e574d22
Guglielmo <sportelli.guglielmo@gmail.com>
2022-10-31 15:49:12 +0100

    created disclaimer component for unverified tokens

--- src/components/modals/swap-modals/SwapSuccesTxView.js
+++ src/components/modals/swap-modals/SwapSuccesTxView.js
@@ -5,7 +5,7 @@ import { extractDecimal, getDecimalPlaces, reduceBalance } from '../../../utils/
 import reduceToken from '../../../utils/reduceToken';
 import { getTokenIconByCode, getTokenName } from '../../../utils/token-utils';
 import GameEditionLabel from '../../game-edition-v2/components/GameEditionLabel';
-import { AlertIcon, ArrowIcon, ChainIcon, NotificationWarningIcon } from '../../../assets';
+import { AlertIcon, ArrowIcon, ChainIcon } from '../../../assets';
 import { CHAIN_ID } from '../../../constants/contextConstants';
 import Label from '../../shared/Label';
 import { FlexContainer } from '../../shared/FlexContainer';
@@ -13,8 +13,8 @@ import CopyPopup from '../../shared/CopyPopup';
 import CustomDivider from '../../shared/CustomDivider';
 import { SuccessViewContainerGE, SuccesViewContainer } from '../TxView';
 import RowTokenInfoPrice from '../../shared/RowTokenInfoPrice';
-import { commonColors, theme } from '../../../styles/theme';
-import styled from 'styled-components';
+import { theme } from '../../../styles/theme';
+import DisclaimerUnverifiedTokens from '../../shared/DisclaimerUnverifiedTokens';
 
 export const SwapSuccessViewGE = () => {
   const { account } = useAccountContext();
@@ -97,22 +97,6 @@ export const SwapSuccessView = ({ loading, sendTransaction, fromValues }) => {
   const swap = useSwapContext();
   const { themeMode } = useApplicationContext();
 
-  const SvgContainer = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 32px;
-    width: 32px;
-    margin-right: 8px;
-    svg {
-      height: 20px;
-      width: 20px;
-      path {
-        fill: ${({ commonColors }) => commonColors.gold}!important;
-      }
-    }
-  `;
-
   const amountBWithSlippage =
     extractDecimal(swap?.localRes?.result?.data[1]?.amount) - extractDecimal(swap?.localRes?.result?.data[1]?.amount) * pact.slippage;
 
@@ -143,17 +127,7 @@ export const SwapSuccessView = ({ loading, sendTransaction, fromValues }) => {
       <FlexContainer className="w-100 column" gap={12}>
         {/* DISCLAIMER */}
         {(!pact.allTokens[getTokenName(swap?.localRes?.result?.data[0]?.token, pact.allTokens)].isVerified ||
-          !pact.allTokens[getTokenName(swap?.localRes?.result?.data[1]?.token, pact.allTokens)].isVerified) && (
-          <FlexContainer className="align-ce justify-sb">
-            <SvgContainer commonColors={commonColors}>
-              <NotificationWarningIcon />
-            </SvgContainer>
-
-            <Label fontSize={12} fontFamily="basier" color={commonColors.gold}>
-              This transaction contains unverified tokens. Review the preview information throughly before confirming the transaction.
-            </Label>
-          </FlexContainer>
-        )}
+          !pact.allTokens[getTokenName(swap?.localRes?.result?.data[1]?.token, pact.allTokens)].isVerified) && <DisclaimerUnverifiedTokens />}
         {/* ACCOUNT */}
         <FlexContainer className="align-ce justify-sb">
           <Label fontSize={13}>Account</Label>
