38e490dcd278119425a2840afb8cd975f66f6e74
Giuseppe Pace <pacepeppe2003@libero.it>
2022-12-23 19:48:50 +0100

    Implemented blacklist functionality and blacklisted lago tokens

--- src/constants/cryptoCurrencies.js
+++ src/constants/cryptoCurrencies.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { tokens } from './tokens';
+import { tokens, blacklistedTokens } from './tokens';
 import { pairs } from './pairs';
 
 const environment = process.env.REACT_APP_KDA_NETWORK_TYPE || 'testnet';
 const tokenData = tokens[environment];
 export const pairsData = pairs[environment];
-
+export const blacklistedTokenData = blacklistedTokens[environment];
 // TODO add img or similar to PactContext -> allTokens
 Object.values(tokenData).forEach((token) => {
   tokenData[token.name].icon = <img alt="" src={tokenData[token.name].icon} style={{ width: 20, height: 20, marginRight: '8px' }} />;
