300d8d0c2fd8f773cde03d43eccf52bff15dbd6f
Giuseppe Pace <pacepeppe2003@libero.it>
2023-01-12 11:06:13 +0100

    New network gas data api + cleaned usage of old API + update env.example

--- src/api/kaddex-be-tools.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const simplexQuote = async (data) => {
-  return axios.post(`${process.env.REACT_APP_KADDEX_API_URL}/api/Simplex/Quote`, data);
-};
-
-export const simplexPaymentRequest = async (data) => {
-  return axios.post(`${process.env.REACT_APP_KADDEX_API_URL}/api/Simplex/PaymentRequest`, data);
-};
