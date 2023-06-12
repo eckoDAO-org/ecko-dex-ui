af5a15abe2f28882bebbcfe736d295f1c168c415
ecko-san <135989259+ecko-san@users.noreply.github.com>
2023-06-12 18:42:13 +0100

    Update AnalyticsInfo.js
    Text changes

--- src/components/stake/AnalyticsInfo.js
+++ src/components/stake/AnalyticsInfo.js
@@ -6,9 +6,9 @@ import Label from '../shared/Label';
 const AnalyticsInfo = () => {
   return (
     <FlexContainer className="column" gap={16}>
-      <Label fontSize={16}>Daily Income</Label>
+      <Label fontSize={16}>Governance Mining Incentives</Label>
       <Label>
-        It is an estimate of the amount of KDX you will get at the end of the day as a reward for your KDX staking position. It comes from the 0.05%
+        It is an estimate of the amount of KDX you will get at the end of the day as a reward for participating in Governance Mining. It comes from the 0.05%
         of the overall DEX trading fees.
       </Label>
       <Divider />
@@ -20,15 +20,15 @@ const AnalyticsInfo = () => {
       <Divider />
       <Label fontSize={16}>KDX Burned</Label>
       <Label>
-        It represents the total amount of KDX that has been burned through penalties on the position or rewards consequent to unstaking within the
+        It represents the total amount of KDX that has been burned through penalties on the position or rewards consequent to unlocking within the
         first 72h or withdrawing rewards within the initial 60-day window, respectively.
       </Label>
       <Divider />
-      <Label fontSize={16}>Staked Share</Label>
-      <Label>Your personal percentage share of KDX amongst all the KDX currently being staked.</Label>
+      <Label fontSize={16}>Locked Share</Label>
+      <Label>Your personal percentage share of KDX amongst all the KDX currently being locked.</Label>
       <Divider />
-      <Label fontSize={16}>Total Staked</Label>
-      <Label>It shows the percentage of KDX being staked with respect to the total KDX circulating supply.</Label>
+      <Label fontSize={16}>Total Locked</Label>
+      <Label>It shows the percentage of KDX being locked with respect to the total KDX circulating supply.</Label>
     </FlexContainer>
   );
 };
