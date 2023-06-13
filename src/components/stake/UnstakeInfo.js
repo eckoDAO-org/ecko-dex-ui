d307a1db4a37eb4dcdf37fbf349f3e71e424003a
ecko-san <135989259+ecko-san@users.noreply.github.com>
2023-06-13 12:05:36 +0100

    Update UnstakeInfo.js
    Text Changes

--- src/components/stake/UnstakeInfo.js
+++ src/components/stake/UnstakeInfo.js
@@ -6,12 +6,12 @@ import List from '../shared/List';
 const UnstakeInfo = () => {
   return (
     <FlexContainer className="column" gap={16}>
-      <Label>Unstaking your funds is always possible, even partially. To further reward long-term holders, the following structure applies:</Label>
+      <Label>Unlocking your funds is always possible, even partially. To further reward long-term holders, the following structure applies:</Label>
       <List
         paddingLeft={16}
         items={[
-          'Unstaking during the first 72 hours will trigger a 3% flat penalty on your unstaked amount.',
-          'The partial or complete unstaking will reset the rewards penalty period from 0.',
+          'Unstaking during the first 72 hours will trigger a 3% flat penalty on your unlocked amount.',
+          'The partial or complete unlocking will reset the rewards penalty period from 0.',
         ]}
       />
     </FlexContainer>
